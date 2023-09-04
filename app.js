a38444256e911888152cec83c929168a4c07f894
David Doll <doll.david0395@gmail.com>
2023-09-04 03:00:06 -0500

    add a playRound function to app.js that will return a string describing the winner of the round based on the player and computers selections

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random() * (3));
+    let computerSelection = Math.floor(Math.random() * (3));
 
-    switch (computerChoice) {
+    switch (computerSelection) {
         case 0:
             return "rock";
         case 1:
@@ -13,5 +13,44 @@ function getComputerChoice() {
     }
 }
 
+function playRound(playerSelection, computerSelection){
+    if(playerSelection === "rock"){
+        switch (computerSelection) {
+            case "rock":
+                return "Tie Match! You both chose Rock.";
+            case "paper":
+                return "You Lose! Paper beats Rock.";
+            case "scissors":
+                return "You Win! Rock beats Scissors.";
+            default:
+                return "Invalid Computer Selection";
+        }
+    }else if(playerSelection === "paper"){
+        switch (computerSelection) {
+            case "rock":
+                return "You Win! Paper beats Rock.";
+            case "paper":
+                return "Tie Match! You both chose Paper.";
+            case "scissors":
+                return "You Lose! Scissors beats Paper.";
+            default:
+                return "Invalid Computer Selection";
+        }
+    }else if(playerSelection === "scissors"){
+        switch (computerSelection) {
+            case "rock":
+                return "You Lose! Rock beats Scissors.";
+            case "paper":
+                return "You Win! Scissors beats Paper.";
+            case "scissors":
+                return "Tie Match! You both chose Scissors.";
+            default:
+                return "Invalid Computer Selection";
+        }
+    }
+    return "Invalid Player Selection Chosen";
+}
+
+console.log(playRound("scissors", getComputerChoice()));
 
 
